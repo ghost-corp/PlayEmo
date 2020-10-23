44799d663649f6e78f466cdda3fff74c47872247
DeTuksa <39960305+DeTuksa@users.noreply.github.com>
2020-10-23 19:36:55 +0100

    onTap function for bottom bar icons

--- lib/widgets/bottom_bar_icons.dart
+++ lib/widgets/bottom_bar_icons.dart
@@ -5,11 +5,12 @@ import 'package:playemo/global/app_color.dart';
 
 class BottomBarIcon extends StatefulWidget {
 
+  final VoidCallback onTap;
   final IconData iconData;
   final String label;
   final bool selected;
 
-  const BottomBarIcon({this.iconData, this.label, this.selected});
+  const BottomBarIcon({this.iconData, this.label, this.selected, this.onTap});
 
   @override
   _BottomBarIconState createState() => _BottomBarIconState();
@@ -18,44 +19,47 @@ class BottomBarIcon extends StatefulWidget {
 class _BottomBarIconState extends State<BottomBarIcon> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          widget.selected ? ShaderMask(
-            child: Icon(
-              widget.iconData,
+    return InkWell(
+      onTap: this.widget.onTap,
+      child: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            widget.selected ? ShaderMask(
+              child: Icon(
+                widget.iconData,
+              ),
+              blendMode: BlendMode.srcIn,
+              shaderCallback: (Rect bounds) {
+                return ui.Gradient.linear(
+                    Offset(4, 24),
+                    Offset(24, 4),
+                    [AppColor.primary, AppColor.primary1]
+                );
+              },
+            ) : Icon(
+              widget.iconData, color: Colors.white,
             ),
-            blendMode: BlendMode.srcIn,
-            shaderCallback: (Rect bounds) {
-              return ui.Gradient.linear(
-                  Offset(4, 24),
-                  Offset(24, 4),
-                  [AppColor.primary, AppColor.primary1]
-              );
-            },
-          ) : Icon(
-            widget.iconData, color: Colors.white,
-          ),
-          widget.selected ? ShaderMask(
-            child: Text(
+            widget.selected ? ShaderMask(
+              child: Text(
+                widget.label,
+              ),
+              blendMode: BlendMode.srcIn,
+              shaderCallback: (Rect bounds) {
+                return ui.Gradient.linear(
+                    Offset(4, 24),
+                    Offset(24, 4),
+                    [AppColor.primary, AppColor.primary1]
+                );
+              },
+            ) : Text(
               widget.label,
-            ),
-            blendMode: BlendMode.srcIn,
-            shaderCallback: (Rect bounds) {
-              return ui.Gradient.linear(
-                  Offset(4, 24),
-                  Offset(24, 4),
-                  [AppColor.primary, AppColor.primary1]
-              );
-            },
-          ) : Text(
-            widget.label,
-            style: TextStyle(color: Colors.white54),
-          )
-        ],
+              style: TextStyle(color: Colors.white54),
+            )
+          ],
+        ),
       ),
     );
   }
