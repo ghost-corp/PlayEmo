2e67bbbda76ce5ff1aba413c1bc0356aa28b5c57
DeTuksa <39960305+DeTuksa@users.noreply.github.com>
2020-11-01 23:20:57 +0100

    page routing animation

--- lib/widgets/playing_widget.dart
+++ lib/widgets/playing_widget.dart
@@ -3,6 +3,8 @@ import 'package:playemo/global/app_color.dart';
 import 'package:playemo/global/device_size.dart';
 import 'dart:ui' as ui;
 
+import 'package:playemo/screens/playing_screen.dart';
+
 class PlayingWidget extends StatefulWidget {
 
   final String imgPath;
@@ -18,89 +20,112 @@ class PlayingWidget extends StatefulWidget {
 class _PlayingWidgetState extends State<PlayingWidget> {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 4,
-      child: Container(
-        padding: EdgeInsets.only(
-          left: 15, right: 15
-        ),
-        width: width(context),
-        height: 70,
-        color: AppColor.darkThemeBackground,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Container(
-                  width: 55, height: 55,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.asset(
-                      widget.imgPath
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      widget.song,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(createRoute());
+      },
+      child: Material(
+        elevation: 4,
+        child: Container(
+          padding: EdgeInsets.only(
+            left: 15, right: 15
+          ),
+          width: width(context),
+          height: 70,
+          color: AppColor.darkThemeBackground,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 55, height: 55,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        widget.imgPath
                       ),
                     ),
-                    SizedBox(height: 5,),
-                    Text(
-                      widget.artist,
-                      style: TextStyle(
-                          color: Colors.white38,
-                          fontSize: 14,
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.play_arrow_sharp,
-                  color: Colors.white,
-                  size: 30,
-                ),
-                SizedBox(width: 10,),
-                ShaderMask(
-                  blendMode: BlendMode.srcIn,
-                  shaderCallback: (Rect bounds) {
-                    return ui.Gradient.linear(
-                      Offset(24,  4),
-                      Offset(4, 24),
-                      [AppColor.primary, AppColor.primary1]
-                    );
-                  },
-                  child: Icon(
-                    Icons.favorite_outlined,
                   ),
-                ),
-                SizedBox(width: 10,),
-                Icon(
-                  Icons.fast_forward,
-                  color: Colors.white,
-                  size: 30,
-                )
-              ],
-            ),
-          ],
+                  SizedBox(width: 10,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        widget.song,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+                      Text(
+                        widget.artist,
+                        style: TextStyle(
+                            color: Colors.white38,
+                            fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.play_arrow_sharp,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  SizedBox(width: 10,),
+                  ShaderMask(
+                    blendMode: BlendMode.srcIn,
+                    shaderCallback: (Rect bounds) {
+                      return ui.Gradient.linear(
+                        Offset(24,  4),
+                        Offset(4, 24),
+                        [AppColor.primary, AppColor.primary1]
+                      );
+                    },
+                    child: Icon(
+                      Icons.favorite_outlined,
+                    ),
+                  ),
+                  SizedBox(width: 10,),
+                  Icon(
+                    Icons.fast_forward,
+                    color: Colors.white,
+                    size: 30,
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+Route createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => PlayingScreen(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      var offsetAnimation = animation.drive(tween);
+      return SlideTransition(
+        position: offsetAnimation,
+        child: child,
+      );
+    },
+    transitionDuration: Duration(milliseconds: 500)
+  );
 }
