1e9fd27d3d8e0917eec169d8d4377d65b20aa67b
DeTuksa <39960305+DeTuksa@users.noreply.github.com>
2020-11-07 22:15:41 +0100

    state management added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:playemo/models/music_model.dart';
 import 'package:playemo/screens/landing.dart';
 import 'package:playemo/screens/song_screen.dart';
 import 'package:playemo/screens/most_popular_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => MusicModel(),
+        )
+      ],
+      child: MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -2,9 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:playemo/global/app_color.dart';
 import 'package:playemo/global/device_size.dart';
+import 'package:playemo/models/music_model.dart';
 import 'package:playemo/screens/home.dart';
+import 'package:playemo/screens/most_popular_screen.dart';
 
 import 'package:playemo/widgets/bottom_bar_icons.dart';
+import 'package:provider/provider.dart';
 
 class LandingScreen extends StatefulWidget {
   @override
@@ -19,114 +22,117 @@ class _LandingScreenState extends State<LandingScreen> {
   bool searchSelected = false;
   bool accountSelected = false;
 
-  var index = 2;
-  var screens = [HomeScreen(), HomeScreen(), HomeScreen(), HomeScreen(), HomeScreen()];
+  var screens = [HomeScreen(), HomeScreen(), HomeScreen(), HomeScreen(), HomeScreen(), MostPopularScreen()];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColor.darkThemeBackground,
-      bottomNavigationBar: Material(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(24),
-          topRight: Radius.circular(24)
-        ),
-        elevation: 2,
-        child: Container(
-          width: width(context),
-          height: 65,
-          decoration: BoxDecoration(
-            color: AppColor.darkThemeBackground,
+    return Consumer<MusicModel>(
+      builder: (context, musicModel, child) {
+        return Scaffold(
+          backgroundColor: AppColor.darkThemeBackground,
+          bottomNavigationBar: Material(
             borderRadius: BorderRadius.only(
-              topRight: Radius.circular(24),
-              topLeft: Radius.circular(24)
-            )
+                topLeft: Radius.circular(24),
+                topRight: Radius.circular(24)
+            ),
+            elevation: 2,
+            child: Container(
+              width: width(context),
+              height: 65,
+              decoration: BoxDecoration(
+                  color: AppColor.darkThemeBackground,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(24),
+                      topLeft: Radius.circular(24)
+                  )
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  BottomBarIcon(
+                    iconData: Icons.my_library_music,
+                    label: 'Playlist',
+                    selected: playListSelected,
+                    onTap: () {
+                      setState(() {
+                        playListSelected = true;
+                        wishListSelected = false;
+                        homeSelected = false;
+                        searchSelected = false;
+                        accountSelected = false;
+                      });
+                      musicModel.changeIndex(0);
+                    },
+                  ),
+                  BottomBarIcon(
+                    iconData: Icons.favorite,
+                    label: 'Wishlist',
+                    selected: wishListSelected,
+                    onTap: () {
+                      setState(() {
+                        playListSelected = false;
+                        wishListSelected = true;
+                        homeSelected = false;
+                        searchSelected = false;
+                        accountSelected = false;
+                      });
+                      musicModel.changeIndex(1);
+                    },
+                  ),
+                  BottomBarIcon(
+                    iconData: CupertinoIcons.double_music_note,
+                    label: 'Home',
+                    selected: homeSelected,
+                    onTap: () {
+                      setState(() {
+                        playListSelected = false;
+                        wishListSelected = false;
+                        homeSelected = true;
+                        searchSelected = false;
+                        accountSelected = false;
+                      });
+                      musicModel.changeIndex(2);
+                    },
+                  ),
+                  BottomBarIcon(
+                    iconData: Icons.search,
+                    label: 'Search',
+                    selected: searchSelected,
+                    onTap: () {
+                      setState(() {
+                        playListSelected = false;
+                        wishListSelected = false;
+                        homeSelected = false;
+                        searchSelected = true;
+                        accountSelected = false;
+                      });
+                      musicModel.changeIndex(3);
+                    },
+                  ),
+                  BottomBarIcon(
+                    iconData: Icons.supervisor_account_sharp,
+                    label: 'Account',
+                    selected: accountSelected,
+                    onTap: () {
+                      setState(() {
+                        playListSelected = false;
+                        wishListSelected = false;
+                        homeSelected = false;
+                        searchSelected = false;
+                        accountSelected = true;
+                      });
+                      musicModel.changeIndex(4);
+                    },
+                  )
+                ],
+              ),
+            ),
           ),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              BottomBarIcon(
-                iconData: Icons.my_library_music,
-                label: 'Playlist',
-                selected: playListSelected,
-                onTap: () {
-                  setState(() {
-                    playListSelected = true;
-                    wishListSelected = false;
-                    homeSelected = false;
-                    searchSelected = false;
-                    accountSelected = false;
-                    index = 0;
-                  });
-                },
-              ),
-              BottomBarIcon(
-                iconData: Icons.favorite,
-                label: 'Wishlist',
-                selected: wishListSelected,
-                onTap: () {
-                  setState(() {
-                    playListSelected = false;
-                    wishListSelected = true;
-                    homeSelected = false;
-                    searchSelected = false;
-                    accountSelected = false;
-                    index = 1;
-                  });
-                },
-              ),
-              BottomBarIcon(
-                iconData: CupertinoIcons.double_music_note,
-                label: 'Home',
-                selected: homeSelected,
-                onTap: () {
-                  setState(() {
-                    playListSelected = false;
-                    wishListSelected = false;
-                    homeSelected = true;
-                    searchSelected = false;
-                    accountSelected = false;
-                    index = 2;
-                  });
-                },
-              ),
-              BottomBarIcon(
-                iconData: Icons.search,
-                label: 'Search',
-                selected: searchSelected,
-                onTap: () {
-                  setState(() {
-                    playListSelected = false;
-                    wishListSelected = false;
-                    homeSelected = false;
-                    searchSelected = true;
-                    accountSelected = false;
-                    index = 3;
-                  });
-                },
-              ),
-              BottomBarIcon(
-                iconData: Icons.supervisor_account_sharp,
-                label: 'Account',
-                selected: accountSelected,
-                onTap: () {
-                  setState(() {
-                    playListSelected = false;
-                    wishListSelected = false;
-                    homeSelected = false;
-                    searchSelected = false;
-                    accountSelected = true;
-                    index = 4;
-                  });
-                },
-              )
-            ],
-          ),
-        ),
-      ),
-      body: screens[index],
+          body: screens[musicModel.index],
+        );
+      },
     );
   }
 }
